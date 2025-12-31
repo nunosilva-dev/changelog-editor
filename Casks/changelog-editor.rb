681f77cd7178b5eca134a568a44d052fc97bcb70
nunosilva <nuno.silva@nau-21.com>
2025-12-31 16:31:05 +0000

    update sha256 for Changelog

--- Casks/changelog-editor.rb
+++ Casks/changelog-editor.rb
@@ -1,6 +1,6 @@
 cask "changelog-editor" do
   version "1.0.0"
-  sha256 "1d09d04172f50a12f3a18371e1225b91ae7e8ff8261853d04dfc2b4e4ca97468"
+  sha256 "f78e1804f608583abb486349328e17da817b411519677486cbbda3457ee98cd8"
 
   url "https://github.com/nunosilva-dev/changelog-editor/releases/download/release/#{version}/Changelog-Editor-#{version}-arm64.dmg"
   name "Changelog Editor"
