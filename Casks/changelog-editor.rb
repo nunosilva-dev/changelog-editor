4a16d08e1bb628c89aa1432fde12ebedb9e1175f
Nuno Silva <nunosilva.code@gmail.com>
2026-01-15 11:59:38 +0000

    Update sha for brew

--- Casks/changelog-editor.rb
+++ Casks/changelog-editor.rb
@@ -1,6 +1,6 @@
 cask "changelog-editor" do
   version "1.1.0"
-  sha256 "f78e1804f608583abb486349328e17da817b411519677486cbbda3457ee98cd8"
+  sha256 "cbc9aeb5d834d6506fe3c860b47596c2e492f741519566566d0102874ffb0ddc"
 
   url "https://github.com/nunosilva-dev/changelog-editor/releases/download/release/#{version}/Changelog-Editor-#{version}-arm64.dmg"
   name "Changelog Editor"
